1ba2ce654f9dee762ab2b9bb669804f9f72fd3ef
Андрей Епифанов <epifanovmd@gmail.com>
2024-06-04 11:07:27 +0300

    add start script

---
+++ start.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+[[ $(docker ps -f name=wireguard -q -a) != '' ]] && docker rm --force $(docker ps -f name=wireguard -q -a)
+docker compose up -d --no-deps --build --force-recreate
+docker image prune -a --force
