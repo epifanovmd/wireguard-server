fa2cad02fcd625f0731ae41f99381515f48299ea
Андрей Епифанов <epifanovmd@gmail.com>
2024-07-25 00:23:10 +0300

    refactoring

--- src/middleware/swagger.middleware.ts
+++ src/middleware/swagger.middleware.ts
@@ -4,6 +4,11 @@ import { koaSwagger } from "koa2-swagger-ui";
 import swaggerDoc from "../../swagger.json";
 
 export const RegisterSwagger = (router: KoaRouter, url: string) => {
+  router.get(url + "/swagger.json", context => {
+    context.status = 200;
+    context.body = swaggerDoc;
+  });
+
   router.get(
     url,
     koaSwagger({
