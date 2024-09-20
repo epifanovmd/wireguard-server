a673ef94283f5069006b0f8f9a095991645540cc
Андрей Епифанов <epifanovmd@gmail.com>
2024-09-21 00:19:36 +0300

    refactoring

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-[[ $(docker ps -f name=wireguard -q -a) != '' ]] && docker rm --force $(docker ps -f name=wireguard -q -a)
-docker compose --env-file .env.production up -d --no-deps --build --force-recreate
-docker image prune -a --force
