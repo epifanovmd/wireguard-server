a983e9f4c1e23c289d8f57ebb77d991d6f486ce0
Андрей Епифанов <epifanovmd@gmail.com>
2024-12-06 23:07:06 +0300

    add dump script

--- src/db/csv_dump/upload.sh
+++
@@ -1,1 +0,0 @@
-scp ./dump.csv root@109.107.189.241:~/auto-sale-server/pgadmin/db_backup/dumps
---
+++ src/db/dump/dump_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SSH_HOST="147.45.133.109"
+TIMESTAMP=$(date +"%F_%H:%M:%S")
+BACKUP_DIR="."
+CONTAINER_NAME="postgres"
+PG_USER="epifanovmd"
+PG_DB="postgres"
+
+ssh  root@$SSH_HOST docker exec $CONTAINER_NAME pg_dump -U $PG_USER -d $PG_DB -F c -b > "$BACKUP_DIR/db_backup_$TIMESTAMP.dump"
